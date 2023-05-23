c64a89c48f3a829defe3ffaae8391ef58c8b491a
giorgio.turtu <giorgio.turtu2@unibo.it>
2023-05-23 19:31:03 +0200

    Minor fixes in setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-import numpy
+import numpy as np
+
+
 try:
-    numpy_include = numpy.get_include()
+    #Return the directory that contains the NumPy *.h header files.
+    #Extension modules that need to compile against NumPy should use this function to locate the appropriate include directory.
+    numpy_include = np.get_include()
 except AttributeError:
-    numpy_include = numpy.get_numpy_include()
+    numpy_include = np.get_numpy_include()
 
 # Handle cython modules
 try:
